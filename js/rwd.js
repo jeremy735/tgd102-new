c7215f98cf3ce1c60943db9c2a7bdad767682819
jeremy735 <xilms735@hotmail.com>
2022-07-28 00:13:25 +0800

    7-28

---
+++ js/rwd.js
@@ -0,0 +1,11 @@
+const hamBar = document.querySelector("a.hamBar");
+hamBar.addEventListener("click", function(e){
+    e.preventDefault();
+const nav = document.querySelector("section.nav");
+    // if(nav.style.display === "none"){
+    //     nav.style.display = "flex"
+    // }else{
+    //     nav.style.display = "none";
+    // }
+    nav.classList.toggle("nav_show")
+});
